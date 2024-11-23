f74b9b442157b56c691553a7691125e65a99c104
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-11-23 17:14:13 -0500

    updates made to vite config and server.ts file for a seamless render deployment. Changes needed to ensure frontend displays during build and deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 const forceDatabaseRefresh = false;
+import path from 'path';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,11 +11,16 @@ import { sequelize } from './models/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+// Serve static files from the entire client's dist folder
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(express.json());
 app.use(routes);
+
+// Catch-all handler for React routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
